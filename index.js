baa95f459fcfdb578caf2e362cc649c3bbc1804f
faiz ahmed choudhary <faiz.ahmed@sourcefuse.com>
2023-01-05 18:00:24 +0530

    second commit

--- index.js
+++ index.js
@@ -1,216 +1,278 @@
-console.log("index.js");
-const formData = () => {
+console.log('Index.js');
+// main class
+// table class all table methods
+// datasource class from where the data is coming
 
-    let dataTable = document.getElementById("dataTable")
-    // json data strats from here 
-    const data = [
-        {
-            "id": 1,
-            "first_name": "faiz ",
-            "middle_name": "ahmed",
-            "last_name": "choudhary",
-            "email": "faiz@mail.com",
-            "phone_number": "8878-86876",
-            "role": "trainee",
-            "address": "Mumbai"
-        },
-        {
-            "id": 2,
-            "first_name": "Zain",
-            "middle_name": "New",
-            "last_name": "Malik",
-            "email": "malik@mail.com",
-            "phone_number": "12321-9887",
-            "role": "Band",
-            "address": "Delhi"
-        },
-        {
-            "id": 3,
-            "first_name": "React",
-            "middle_name": "JS",
-            "last_name": "Native",
-            "email": "react@mail.com",
-            "phone_number": "0987-564321",
-            "role": "Developer",
-            "address": "USA"
+const jsonformdata = () => {
+    loadtable = document.getElementById('loadtable');
+    loadData = document.getElementById('loadData');
+
+    clearBtn = document.querySelectorAll('.clear-btn');
+
+    loadData.innerText = 'Refresh Data'
+    console.log('json form data() clicked');
+
+    class main {
+
+    }
+
+    class dataSource {
+        getdata() {
+            // console.log('getdata')
+            return [
+                {
+                    "id": 1,
+                    "first_name": "faiz ",
+                    "middle_name": "ahmed",
+                    "last_name": "choudhary",
+                    "email": "faiz@mail.com",
+                    "phone_number": "8878-86876",
+                    "role": "trainee",
+                    "address": "Mumbai"
+                },
+                {
+                    "id": 2,
+                    "first_name": "Zain",
+                    "middle_name": "New",
+                    "last_name": "Malik",
+                    "email": "malik@mail.com",
+                    "phone_number": "12321-9887",
+                    "role": "Band",
+                    "address": "Delhi"
+                },
+                {
+                    "id": 3,
+                    "first_name": "React",
+                    "middle_name": "JS",
+                    "last_name": "Native",
+                    "email": "react@mail.com",
+                    "phone_number": "0987-564321",
+                    "role": "Developer",
+                    "address": "USA"
+                }
+            ]
         }
-    ]
-    // json data ends here
+    }
 
-    // now create a dynamic header starts form here
-    let headerdata = [];
-    for (let i = 0; i < data.length; i++) {
-        for (const key in data[i]) {
-            // console.log("key" , key)
-            // means the header data is black
-            if (headerdata.indexOf(key) === -1) {
-                // console.log(key)
-                headerdata.push(key)
+    class Tableclass {
+
+        constructor(data) {
+            this.data = data
+            // const globaldata = data.getdata()
+        }
+
+        view() {
+            loadtable.innerHTML = ''
+            // console.log('getdata')
+            // console.log(data.getdata())
+            const tableData = data.getdata()
+
+            // now let create a table header
+            var tableheader = [];
+            for (let i = 0; i < tableData.length; i++) {
+                for (const key in tableData[i]) {
+                    // console.log(tableData[i])
+                    if (tableheader.indexOf(key) === -1) {
+                        tableheader.push(key)
+                    }
+                    // console.log(tableheader)
+                }
+            }
+
+            // creating a dynamic table
+            let Createtable = document.createElement('table')
+            Createtable.classList.add('table', 'container')
+            loadtable.append(Createtable)
+
+            // creating a dynamic header for table
+            let Createthead = document.createElement('thead')
+            Createthead.classList.add('thead')
+
+            // lets insert a row in header
+            let theadtr = Createthead.insertRow(-1)
+            for (let i = 0; i < tableheader.length; i++) {
+                let theadrow = document.createElement('th')
+                theadrow.setAttribute('id', 'theadrow-' + i)
+                theadrow.append(tableheader[i])
+                theadtr.append(theadrow)
+                theadtr.setAttribute('id', 'theadtr-' + i)
+            }
+
+            let actionbtn = theadtr.insertCell(-1)
+            actionbtn.innerText = 'Action'
+
+            Createtable.append(Createthead)
+
+            // creating a dynamic body for table
+            let Createtbody = document.createElement('tbody')
+            Createtbody.classList.add('tbody')
+            Createtable.append(Createtbody)
+
+            for (let i = 0; i < tableData.length; i++) {
+                let tbodyrow = Createtbody.insertRow(0)
+                Createtbody.append(tbodyrow)
+                tbodyrow.setAttribute('id', 'tbodyrow-' + i)
+
+                for (let j = 0; j < tableheader.length; j++) {
+                    let tablecell = tbodyrow.insertCell(-1)
+                    //     // tbodyrow.append(tablecell)
+                    tablecell.innerText = tableData[i][tableheader[j]]
+                    // console.log(j)
+                    tablecell.classList.add('tcell')
+                }
+
+                // now lets create a edit button 
+                let editButton = document.createElement('button')
+                editButton.classList.add('edit-btn', 'btn-info')
+                editButton.innerText = 'Edit';
+                let trid = 'tbodyrow-' + i
+                editButton.addEventListener("click", table.edit(trid , tableheader));
+                let editcell = tbodyrow.insertCell(-1)
+                editcell.setAttribute('id' , 'savecleartd')
+                editcell.append(editButton)
+                tbodyrow.append(editcell)
+
+                //  delete button
+                let deletebtn = document.createElement('button')
+                deletebtn.classList.add("btn-danger", "delete-btn", 'ml-2');
+                deletebtn.innerHTML = 'Delete';
+                deletebtn.addEventListener("click", table.delete(trid));
+                deletebtn.setAttribute('id', 'deletebtn' + i)
+                editcell.append(deletebtn)
+
+                // save button
+                let savebutton = document.createElement('button');
+                savebutton.setAttribute('id', 'save-button-' + i);
+                savebutton.innerText = 'Save';
+                savebutton.classList.add('btn-primary', 'save');
+                savebutton.addEventListener("click", table.save(trid , tableheader));
+                savebutton.style.display = 'none';
+
+                // clear button
+                let clearbutton = document.createElement('button');
+                clearbutton.setAttribute('id', 'clear-button-' + i);
+                clearbutton.innerText = 'Clear';
+                clearbutton.classList.add('btn-info', 'clear', 'ml-2');
+                clearbutton.addEventListener("click", table.clear(trid , tableheader, tableData));
+                clearbutton.style.display = 'none';
+
+                // let tablelastcell = tbodyrow.insertCell(-1);
+                editcell.append(savebutton)
+                editcell.append( clearbutton);
+
+            }
+
+        }
+
+        // edit
+        edit(id , tableheader) {
+            return function () {
+                // console.log(id)
+                let editelem = document.getElementById(id)
+                editelem.style.backgroundColor = '#E2C4B8';
+
+                for (let i = 0; i < tableheader.length; i++) {
+                    let getcell = editelem.querySelectorAll('.tcell')[i]
+                    getcell.setAttribute('contenteditable' , 'true')
+                    // console.log(getcell)    
+                }
+
+                // lets toggle buttons
+                for (let j = 0; j < editelem.querySelector('#savecleartd').children.length; j++) {  
+                    // console.log("editelem.querySelector('#savecleartd').children[i]")
+                    // console.log(j)
+                    console.log(editelem.querySelector('#savecleartd').children[j])
+
+                    if (editelem.querySelector('#savecleartd').children[j].style.display === 'none') {
+                        editelem.querySelector('#savecleartd').children[j].style.display = 'block'
+                    } else {
+                        editelem.querySelector('#savecleartd').children[j].style.display = 'none'
+
+                    }
+                }                
             }
         }
 
-    }
-
-    // lets create a table
-    const table = document.createElement('table');
-    table.classList.add("data-table")
-
-    // create a table header 
-    const thead = document.createElement('thead')
-    table.append(thead);
-
-    // create a row in thead
-    let theadrow = thead.insertRow(0)
-
-    // now lets add data to it
-    for (let i = 0; i < headerdata.length; i++) {
-        th = document.createElement('th');
-        th.append(headerdata[i])
-        theadrow.append(th);
-    }
-    action = theadrow.insertCell(-1)
-    action.innerHTML = `<th>Action</th>`
-
-    // now lets create table cell and data to it
-    const tbody = document.createElement('tbody');
-
-    table.append(tbody)
-
-    dataTable.innerHTML = ''
-    document.querySelector('.showdata-btn').innerText = 'Refresh  data';
-    dataTable.append(table)
-
-    // now add value form data in column
-    for (let i = 0; i < data.length; i++) {
-        let tbodyrow = tbody.insertRow(0)
-        tbodyrow.setAttribute('id', 'tbody-tr-' + i)
-        tbodyrow.classList.add('tbodyrow')
-        tbody.append(tbodyrow)
-
-        for (let j = 0; j < headerdata.length; j++) {
-            let tcell = tbodyrow.insertCell(-1)
-            // console.log("i is" , i)
-            // console.log("j is" , j)
-            // console.log("j is" , headerdata[j])
-            // console.log("data[i][headerdata[j] is" , data[i][headerdata[j]])
-            tcell.innerText = data[i][headerdata[j]]
-            tcell.classList.add('tcell-editable')
-
+        // delete
+        delete(id) {
+            return function () {
+                console.log(id)
+                let delelem = document.getElementById(id)
+                delelem.remove()   
+            }
         }
 
-        // now let create a edit and a delte button for every row 
-        editCell = tbodyrow.insertCell(-1)
-        // edit btn
-        editbutton = document.createElement('button');
-        editbutton.setAttribute('id', 'save-button-' + i);
-        editbutton.innerText = 'Edit';
-        editbutton.classList.add('edit-btn', 'btn-dark', 'mr-2');
+        // clear
+        clear(id , tableheader, tableData) {
+            return function () {
+             console.log('clear')
+                 let clearelem = document.getElementById(id);
+                 let clearelemindex = document.getElementById(id).rowIndex;
+                 console.log(clearelemindex)
+                 
+                 clearelem.style.backgroundColor = '#fff';
+                 
+                 for (let i = 0; i < tableData.length; i++) {
+                     
+                     for (let j = 0; j < tableheader.length; j++) {
+                         let content = tableData[clearelemindex - 1][tableheader[j]];
+                          clearelem.querySelectorAll('.tcell')[j].innerText = content;
+                    }
+                    
+                 }
 
-        // let edit = document.querySelectorAll('.edit-btn')
-        $(document).ready(function () {
-            $('.edit-btn').click(function () {
-                console.log('k')
-                $(this).closest('tr').find('.tcell-editable').attr('contenteditable', 'true');
-                $(this).closest('tr').css("background-color", "#E2C4B8");
-                $(this).closest('tr').find('.clear').css("display", "block");
-                $(this).closest('tr').find('.save').css("display", "block");
-            })
-        })
+                   // lets toggle buttons
+                   for (let j = 0; j < clearelem.querySelector('#savecleartd').children.length; j++) {  
+                    // console.log("editelem.querySelector('#savecleartd').children[i]")
+                    // console.log(j)
+                    console.log(clearelem.querySelector('#savecleartd').children[j])
 
-        editCell.append(editbutton);
+                    if (clearelem.querySelector('#savecleartd').children[j].style.display === 'none') {
+                        clearelem.querySelector('#savecleartd').children[j].style.display = 'block'
+                    } else {
+                        clearelem.querySelector('#savecleartd').children[j].style.display = 'none'
 
-        // delete btn
-        deletebtn = document.createElement('button')
-        deletebtn.classList.add("btn-danger", "delete-btn");
-        deletebtn.innerHTML = 'Delete';
-        deletebtn.setAttribute('id', 'deletebtn' + i)
-
-        // delete btn functionality
-        $(document).ready(function () {
-
-            $('.delete-btn').click(function () {
-                console.log('${this}');
-                $(this).closest('tr').hide()
-            })
-
-        })
-
-
-        editCell.append(deletebtn)
+                    }
+                }
+             
+            } 
+         }
 
 
-        // lets create a save & cancel btn
-        savebutton = document.createElement('button');
-        savebutton.setAttribute('id', 'save-button-' + i);
-        savebutton.innerText = 'Save';
-        savebutton.classList.add('btn-primary', 'save');
-        savebutton.style.display = 'none'
-        trt = tbodyrow.insertCell(-1);
-        trt.append(savebutton);
+        // save
+        save(id , tableheader) {
+           return function () {
+            // console.log(id)
+                let saveelem = document.getElementById(id)
+                saveelem.style.backgroundColor = '#fff';
 
-        $('.save').click(function () {
-            $(this).closest('tr').find('.tcell-editable').attr('contenteditable', 'false');
-            $(this).closest('tr').css("background-color", "#fff");
-            // alert('changes saved')
-            $(this).closest('tr').find('.clear').css('display', 'none');
-            $(this).closest('tr').find('.save').css('display', 'none');
-        })
+                for (let i = 0; i < tableheader.length; i++) {
+                    let getcell = saveelem.querySelectorAll('.tcell')[i]
+                    getcell.setAttribute('contenteditable' , 'false')
+                    // console.log(getcell)    
+                }
 
-        // cancel
-        clearbutton = document.createElement('button');
-        clearbutton.setAttribute('id', 'clear-button-' + i);
-        clearbutton.innerText = 'Clear';
-        clearbutton.classList.add('btn-info', 'clear');
-        clearbutton.style.display = 'none'
-        trt.append(clearbutton);
+                
+                   // lets toggle buttons
+                   for (let j = 0; j < saveelem.querySelector('#savecleartd').children.length; j++) {  
+                    // console.log("editelem.querySelector('#savecleartd').children[i]")
+                    // console.log(j)
+                    console.log(saveelem.querySelector('#savecleartd').children[j])
 
+                    if (saveelem.querySelector('#savecleartd').children[j].style.display === 'none') {
+                        saveelem.querySelector('#savecleartd').children[j].style.display = 'block'
+                    } else {
+                        saveelem.querySelector('#savecleartd').children[j].style.display = 'none'
 
-        $('.clear').click(function () {
-            let ctablerow = $(this).closest('tr')[0].rowIndex;
-            console.log(ctablerow)
-
-            for (let k = 0; k < headerdata.length; k++) {
-                let content = data[ctablerow - 1][headerdata[k]];
-                $(this).closest('tr').children('.tcell-editable').eq(k).text(content)
-            }
-            $(this).closest('tr').find('.tcell-editable').attr('contenteditable', 'false');
-            $(this).closest('tr').find('.clear').css('display', 'none');
-            $(this).closest('tr').find('.save').css('display', 'none');
-            $(this).closest('tr').css("background-color", "#fff");
-
-        })
-
-
+                    }
+           } 
+        }
     }
+    }
+    let data = new dataSource();
+    // console.log(data.getdata())
 
-
-    // let create a show data button
-    // main table datasource 
-
-    // showdata = document.createElement('button')
-    // showdata.classList.add("showdata-btn", 'btn-dark');
-    // showdata.innerText = 'Load Data';
-    // dataTable.append(showdata);
-
-    // document.querySelector('.showdata-btn').addEventListener('click', () => {
-    //     console.log('show btn clicked')
-
-    // })
-
+    let table = new Tableclass(data)
+    table.view()
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
